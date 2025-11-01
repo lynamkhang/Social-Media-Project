307987e7d4376244f1aed97394862d3f512849dd
lynamkhang <lykhang14122003@gmail.com>
2025-11-01 22:16:15 +0700

    update authorization

---
+++ backend/src/middlewares/authMiddleware.js
@@ -0,0 +1,42 @@
+import jwt from 'jsonwebtoken';
+import User from '../models/User.js'
+
+//authorization
+export const protectedRoute = (req, res, next) => {
+    try {
+        //Get token from header
+        const authHeader = req.headers['authorization'];
+        const token = authHeader && authHeader.split(" ")[1];
+
+        if(!token) {
+            return res.status(401).json({message: "Access token not found"});
+        }
+
+        //Confirm valid token
+        jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, async(err, decodedUser) => {
+            if(err) {
+                console.error(err);
+
+                return res.status(403).json({message: "Access token expired or incorrect"});
+            }
+
+            
+            //Find user
+            const user = await User.findById(decodedUser.userId).select('-passwordHash');
+
+            if(!user) {
+                return res.status(404).json({message: "User not found."});
+            }
+
+            //Return user to req
+            req.user = user;
+            next();
+        });
+
+    } catch (error) {
+        console.error('Error when JWT authorize in authMiddleware', error);
+        return res
+        .status(500)
+        .json({message:'System error'});
+    }
+} 
--- backend/src/server.js
+++ backend/src/server.js
@@ -1,7 +1,10 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import { connectDB } from './libs/db.js';
-import authRoute from './routes/authRoute.js'
+import authRoute from './routes/authRoute.js';
+import userRoute from './routes/userRoute.js';
+import cookieParser from 'cookie-parser';
+import { protectedRoute } from './middlewares/authMiddleware.js';
 
 dotenv.config();
 
@@ -13,8 +16,11 @@ app.use(express.json());
 
 //public routes
 app.use('/api/auth', authRoute);
+app.use(cookieParser());
 
 //private route
+app.use(protectedRoute);
+app.use('/api/users', userRoute);
 
 connectDB().then(() => {
     app.listen(PORT, () => {
